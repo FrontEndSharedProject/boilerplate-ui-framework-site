aec6925055a315d824e01cc718086e257ed1c9f8
carl-jin <carl-jin@users.noreply.github.com>
2021-10-22 19:27:18 +0000

    deploy: carl-jin/boilerplate-ui-framework@1eebe3293257eaccf6d87db168b3aef8b8cb2fa1

---
+++ assets/js/54ada242.9ab5d5b7.js
@@ -0,0 +1,23 @@
+"use strict";
+(self["webpackChunkboilerplate_ui_framework"] = self["webpackChunkboilerplate_ui_framework"] || []).push([[687],{
+
+/***/ 9943:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "frontMatter": function() { return /* binding */ frontMatter; },
+/* harmony export */   "contentTitle": function() { return /* binding */ contentTitle; },
+/* harmony export */   "metadata": function() { return /* binding */ metadata; },
+/* harmony export */   "toc": function() { return /* binding */ toc; },
+/* harmony export */   "default": function() { return /* binding */ MDXContent; }
+/* harmony export */ });
+/* harmony import */ var _home_runner_work_boilerplate_ui_framework_boilerplate_ui_framework_node_modules_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(7462);
+/* harmony import */ var _home_runner_work_boilerplate_ui_framework_boilerplate_ui_framework_node_modules_babel_runtime_helpers_esm_objectWithoutPropertiesLoose__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3366);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7294);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3905);
+var _excluded=["components"];var frontMatter={sidebar_position:6,title:'自动部署'};var contentTitle=undefined;var metadata={"unversionedId":"advance/deploy","id":"advance/deploy","isDocsHomePage":false,"title":"自动部署","description":"配置","source":"@site/docs/advance/deploy.mdx","sourceDirName":"advance","slug":"/advance/deploy","permalink":"/boilerplate-ui-framework-site/docs/advance/deploy","tags":[],"version":"current","sidebarPosition":6,"frontMatter":{"sidebar_position":6,"title":"自动部署"},"sidebar":"tutorialSidebar","previous":{"title":"打包后的使用","permalink":"/boilerplate-ui-framework-site/docs/advance/use-as-a-package"},"next":{"title":"Card 组件","permalink":"/boilerplate-ui-framework-site/docs/components/card"}};/* @jsxRuntime classic */ /* @jsx mdx */var toc=[{value:'配置',id:'配置',children:[],level:2}];var layoutProps={toc:toc};var MDXLayout="wrapper";function MDXContent(_ref){var components=_ref.components,props=(0,_home_runner_work_boilerplate_ui_framework_boilerplate_ui_framework_node_modules_babel_runtime_helpers_esm_objectWithoutPropertiesLoose__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z)(_ref,_excluded);return (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .mdx */ .kt)(MDXLayout,(0,_home_runner_work_boilerplate_ui_framework_boilerplate_ui_framework_node_modules_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z)({},layoutProps,props,{components:components,mdxType:"MDXLayout"}),(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .mdx */ .kt)("h2",{"id":"配置"},"\u914D\u7F6E"),(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .mdx */ .kt)("p",null,(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .mdx */ .kt)("a",{parentName:"p","href":"https://docusaurus.io/zh-CN/docs/deployment"},"https://docusaurus.io/zh-CN/docs/deployment")),(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .mdx */ .kt)("p",null,"\u5982\u679C\u4F60\u8981\u90E8\u7F72\u5230\u7F51\u7AD9\u4E0A\u53BB, \u8BF7\u52A1\u5FC5\u5C06\u4EE5\u4E0B\u4FE9\u914D\u7F6E\u8BBE\u7F6E\u597D"),(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .mdx */ .kt)("pre",null,(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .mdx */ .kt)("code",{parentName:"pre","className":"language-javascript","metastring":"title=\"docusaurus.config.js\"","title":"\"docusaurus.config.js\""},"{\n  url: \"https://carl-jin.github.io\",\n  baseUrl: \"/boilerplate-ui-framework-site/\"\n}\n")));};MDXContent.isMDXComponent=true;
+
+/***/ })
+
+}]);
